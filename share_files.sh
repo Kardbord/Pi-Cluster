ef32a2d8027b7c59ba05f9ae21818fe669b81e54
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-11-17 17:58:58 -0700

    initial commit

---
+++ share_files.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+HOSTFILE="./hostfile"
+
+share_files () {
+    while read node; do
+        scp  "$1" "$node:/home/test/"
+    done < $HOSTFILE
+}
+
+if [[ "$#" -lt 1 || ( "$1" != *.cpp && "$1" != *.py ) ]]; then
+
+    echo -e "\nProvide a cpp or py file.\n"
+    exit 0
+
+fi
+
+if [[ "$1" == *.cpp ]]; then
+    # CPP FILE PROVIDED
+    
+    mpic++ "$1" || exit 1
+    #rm "$1"
+
+    share_files "a.out"
+
+    exit 0
+
+else
+    # PY FILE PROVIDED
+
+    share_files "$1"
+
+fi
